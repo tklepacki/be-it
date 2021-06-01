0d4b68a5f2fa532d76e456ba59f33d1d80e66c65
Hanna Kaniewska <hanna.kaniewska@gmail.com>
2021-06-01 15:33:56 +0200

    Parallelization in TestNG

--- src/main/java/stores/reserved/page/NewsletterPopUpPage.java
+++ src/main/java/stores/reserved/page/NewsletterPopUpPage.java
@@ -2,20 +2,28 @@ package stores.reserved.page;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.ScrollToElement;
 import utils.Wait;
 
 public class NewsletterPopUpPage {
 
+	private final By newsletterPopUpLocator = By.xpath("//*[@id='newsletterContainer']/div[@class='close']");
+
 	private WebDriver driver;
+	private ScrollToElement scroll;
 
 	public NewsletterPopUpPage(WebDriver driver) {
 		this.driver = driver;
+		this.scroll = new ScrollToElement(driver);
 	}
 
 	public void closeNewsletterPopUp() {
 		Wait.waitForThreeSeconds();
-		if (!driver.findElements(By.xpath("//*[@id='newsletterContainer']/div[@class='close']")).isEmpty()) {
-			driver.findElement(By.xpath("//*[@id='newsletterContainer']/div[@class='close']")).click();
+		if (!driver.findElements(newsletterPopUpLocator).isEmpty()) {
+			WebElement newsletterPopUp = driver.findElement(newsletterPopUpLocator);
+			scroll.scrollToElement(newsletterPopUp);
+			newsletterPopUp.click();
 		}
 	}
 }
